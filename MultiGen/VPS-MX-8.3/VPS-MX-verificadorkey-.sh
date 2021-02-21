0098a6e30551a7ba4d560ae21fbcebd84be75487
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-02-20 19:43:35 -0500

    Update VPS-MX-verificadorkey-.sh

--- MultiGen/VPS-MX-8.3/VPS-MX-verificadorkey-.sh
+++ MultiGen/VPS-MX-8.3/VPS-MX-verificadorkey-.sh
@@ -370,9 +370,9 @@ wget -O $HOME/lista-arq ${REQUEST}/lista-arq > /dev/null 2>&1 && echo -e "\033[1
 #   echo -e "\033[1;91m Key Incompleta"
 #   invalid_key
 #   exit
-    instalfin
-    sleep 2s
-   }
+instalfin
+sleep 2s
+}
 IP="192.168.43.1" && echo "$IP" > /usr/bin/vendor_code
 sleep 1s
 #function_verify
@@ -380,42 +380,42 @@ updatedb
 #if [[ -e $HOME/lista-arq ]]; then
 #if [[ -e $HOME/lista-arq ]] && [[ ! $(cat $HOME/lista-arq|grep "menu") ]]; then
 instalfin () {
-   msg -bar2
-   msg -verd "$(source trans -b es:${id} " INSTALANDO"|sed -e 's/[^a-z -]//ig'): \033[1;31m[VPS-MX #MOD by @Kalix1]"
-   REQUEST=$(echo $SCPresq|base64 -d)
-   [[ ! -d ${SCPinstal} ]] && mkdir ${SCPinstal}
-   pontos="."
-   stopping="$(source trans -b es:${id} "Verificando Actualizaciones"|sed -e 's/[^a-z -]//ig')"
-   for arqx in $(cat $HOME/lista-arq); do
-   msg -verm "${stopping}${pontos}"
+msg -bar2
+msg -verd "$(source trans -b es:${id} " INSTALANDO"|sed -e 's/[^a-z -]//ig'): \033[1;31m[VPS-MX #MOD by @Kalix1]"
+REQUEST=$(echo $SCPresq|base64 -d)
+[[ ! -d ${SCPinstal} ]] && mkdir ${SCPinstal}
+pontos="."
+stopping="$(source trans -b es:${id} "Verificando Actualizaciones"|sed -e 's/[^a-z -]//ig')"
+for arqx in $(cat $HOME/lista-arq); do
+msg -verm "${stopping}${pontos}"
    #wget --no-check-certificate -O ${SCPinstal}/${arqx} ${IP}:81/${REQUEST}/${arqx} > /dev/null 2>&1 && verificar_arq "${arqx}" || error_fun
-   wget --no-check-certificate -O ${SCPinstal}/${arqx} ${REQUEST}/${arqx} > /dev/null 2>&1 && verificar_arq "${arqx}" || error_fun
-   tput cuu1 && tput dl1
-   pontos+="."
-   done
-   sleep 1s
-   msg -bar2
-   listaarqs="$(locate "lista-arq"|head -1)" && [[ -e ${listaarqs} ]] && rm $listaarqs   
-   cat /etc/bash.bashrc|grep -v '[[ $UID != 0 ]] && TMOUT=15 && export TMOUT' > /etc/bash.bashrc.2
-   echo -e '[[ $UID != 0 ]] && TMOUT=15 && export TMOUT' >> /etc/bash.bashrc.2
-   mv -f /etc/bash.bashrc.2 /etc/bash.bashrc
-   echo "${SCPdir}/menu" > /usr/bin/menu && chmod +x /usr/bin/menu
-   echo "${SCPdir}/menu" > /usr/bin/adm && chmod +x /usr/bin/adm
-   echo "$Key" > ${SCPdir}/key.txt
-   cd /etc/newadm
-   wget https://www.dropbox.com/s/k92gddhdnla47nl/pytransform.tar >/dev/null 2>&1
-   tar -xf pytransform.tar > /dev/null 2>&1 
-   rm -rf pytransform.tar > /dev/null 2>&1
-   cd
-   [[ -d ${SCPinstal} ]] && rm -rf ${SCPinstal}   
-   [[ ${#id} -gt 2 ]] && echo "es" > ${SCPidioma} || echo "${id}" > ${SCPidioma}
-   echo -e "${cor[2]}         DESEAS INSTALAR NOTI-BOT?(Default n)"
-   echo -e "\033[1;34m  (Deves tener Telegram y el BOT: @Noti_VPSMX_Bot)"
-   msg -bar2
-   read -p " [ s | n ]: " NOTIFY   
-   [[ "$NOTIFY" = "s" || "$NOTIFY" = "S" ]] && NOTIFY
-   msg -bar2
-   [[ ${byinst} = "true" ]] && install_fim
+wget --no-check-certificate -O ${SCPinstal}/${arqx} ${REQUEST}/${arqx} > /dev/null 2>&1 && verificar_arq "${arqx}" || error_fun
+tput cuu1 && tput dl1
+pontos+="."
+done
+sleep 1s
+msg -bar2
+listaarqs="$(locate "lista-arq"|head -1)" && [[ -e ${listaarqs} ]] && rm $listaarqs   
+cat /etc/bash.bashrc|grep -v '[[ $UID != 0 ]] && TMOUT=15 && export TMOUT' > /etc/bash.bashrc.2
+echo -e '[[ $UID != 0 ]] && TMOUT=15 && export TMOUT' >> /etc/bash.bashrc.2
+mv -f /etc/bash.bashrc.2 /etc/bash.bashrc
+echo "${SCPdir}/menu" > /usr/bin/menu && chmod +x /usr/bin/menu
+echo "${SCPdir}/menu" > /usr/bin/adm && chmod +x /usr/bin/adm
+echo "$Key" > ${SCPdir}/key.txt
+cd /etc/newadm
+wget https://www.dropbox.com/s/k92gddhdnla47nl/pytransform.tar >/dev/null 2>&1
+tar -xf pytransform.tar > /dev/null 2>&1 
+rm -rf pytransform.tar > /dev/null 2>&1
+cd $SCPinstal}
+[[ -d ${SCPinstal} ]] && rm -rf ${SCPinstal}   
+[[ ${#id} -gt 2 ]] && echo "es" > ${SCPidioma} || echo "${id}" > ${SCPidioma}
+echo -e "${cor[2]}         DESEAS INSTALAR NOTI-BOT?(Default n)"
+echo -e "\033[1;34m  (Deves tener Telegram y el BOT: @Noti_VPSMX_Bot)"
+msg -bar2
+read -p " [ s | n ]: " NOTIFY   
+[[ "$NOTIFY" = "s" || "$NOTIFY" = "S" ]] && NOTIFY
+msg -bar2
+[[ ${byinst} = "true" ]] && install_fim
 }
 #else
 #invalid_key
